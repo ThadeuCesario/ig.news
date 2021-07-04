1ace45d20bf8b5846598041876310b4526edb2e7
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-07-04 20:56:47 -0300

    implementing _document.tsx component - similar to index.html when we use CRA  🤯

---
+++ src/pages/_document.tsx
@@ -0,0 +1,8 @@
+import Document from 'next/document';
+export default class MyDocument extends Document{
+    render() {
+        return (
+            <div>MyDocument Componente - Será bem parecido com um index.html</div>
+        )
+    }
+}
