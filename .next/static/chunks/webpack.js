00f12649701e55cf3fc3b427a904aa870edcc1a4
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-07-04 20:05:14 -0300

    inserting sass in ignews project

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e38e0b32cf2d375c4ffd"; }
+/******/ 		__webpack_require__.h = function() { return "1299f1abdbfa931b9b7c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
