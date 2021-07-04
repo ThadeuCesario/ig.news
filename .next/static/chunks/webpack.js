0e0d14bfbfea9ef735d75bfc9e09b6045f51e5df
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-07-04 18:59:02 -0300

    implemeting products page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7761cc38ad57c692299b"; }
+/******/ 		__webpack_require__.h = function() { return "6ccbaa2efd7a73a795a6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
