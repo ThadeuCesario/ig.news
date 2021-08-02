3278451eccda17db3dfae61c74241cf6e2d434e2
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-08-02 13:23:00 -0300

    implementing global styles in ignews project

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d832a41eb95a3bb5df8b"; }
+/******/ 		__webpack_require__.h = function() { return "989af466eeb53fe4c7d9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
