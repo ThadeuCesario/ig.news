18c49e45e051cf3019a989b302de614b0525928d
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-08-15 20:26:50 -0300

    Implementing SignIButton component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c566b21dd3bec6b80d23"; }
+/******/ 		__webpack_require__.h = function() { return "38e668ba22fa871b0900"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,12 @@
+import {FaGithub} from 'react-icons/fa';
+
+const SignInButton = () => {
+    return (
+        <button type="button">
+            <FaGithub />
+            Sign in with Github
+        </button>
+    )
+}
+
+export default SignInButton;
