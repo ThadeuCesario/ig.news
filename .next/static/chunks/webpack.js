aa89449cb5e7dbe8bd1e4b7627c5a15d64af71c9
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-11-23 07:51:18 -0300

    implementing Header Component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a42decb74871d159"; }
+/******/ 		__webpack_require__.h = function() { return "86173c142e9b968e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
