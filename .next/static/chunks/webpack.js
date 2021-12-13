e6db90f3743c2521211a7decdae5a9cf92991dfe
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-12-13 07:26:25 -0300

    learning about server site generation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2c03edef8b82c023"; }
+/******/ 		__webpack_require__.h = function() { return "5346a2cb5dace2ef"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
