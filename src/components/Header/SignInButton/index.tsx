ac560af9f06339d558788e84f312199a808469da
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-12-24 06:56:44 -0300

    implementing Github auth integration

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,21 +1,30 @@
 import {FaGithub} from 'react-icons/fa';
 import {FiX} from 'react-icons/fi';
+import {signIn, signOut, useSession} from 'next-auth/react'
 import styles from './styles.module.scss'; 
 
 const SignInButton = () => {
-    const isUserLoggedIn = false;
+    const {data: session} = useSession(); 
 
-    return isUserLoggedIn ? (
+    return session ? (
         (
-            <button className={styles.signInButton} type="button">
+            <button 
+                className={styles.signInButton} 
+                type="button"
+                onClick={() => signOut()}
+            >
                 <FaGithub color="#04d361" />
-                Thadeu Cesário
+                {session.user.name}
                 <FiX color="#737380" className={styles.closeIcon}/>
             </button>
         )
     ) : (
         (
-            <button className={styles.signInButton} type="button">
+            <button 
+                className={styles.signInButton} 
+                type="button"
+                onClick={() => signIn('github')}
+            >
                 <FaGithub color="#eba417" />
                 Sing in with Github
             </button>
