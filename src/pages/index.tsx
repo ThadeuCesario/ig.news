e25051964b2cb08aec61c5b90d3171805a4cb51a
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2022-01-04 06:41:06 -0300

    backing initial project - installing sass and typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,7 @@
-import Head from 'next/head';
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <>
-    <Head>
-    <title>Início | IgNews</title>
-    </Head>
-    <h1>
-      Hello World
-    </h1>
-    </>
+    <h1 className={styles.title} >Hello <span>World</span></h1>
   )
 }
