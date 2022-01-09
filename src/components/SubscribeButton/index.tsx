6b52c29cbe072a9f0286f1c7045c06334d5ce99d
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2022-01-09 07:54:11 -0300

    new treats in subscribe stripe api and prevent stripe to duplicate user.. using faunadb

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,17 +1,26 @@
 import { useSession, signIn } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 const SubscribeButton = () => {
-    const [session]: any = useSession();
+    const {data: session} = useSession(); 
 
-    function handleSubscribe(){
+    async function handleSubscribe(){
         if(!session){
             signIn('github');
             return;
         }
 
         //criação da checkout session
-
+        try{
+            const response = await api.post('/subscribe');
+            const {sessionId} = response.data;
+            const stripe = await getStripeJs();
+            await stripe.redirectToCheckout({sessionId});
+        }catch(err){
+            console.error(err);
+        }
     }
 
     return (
