bb8b90ef9be9b19708262ef569ab20a92268f5ff
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2022-01-09 20:37:09 -0300

    implementing the integration with prismic cms - get the posts

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "40ddfb02341085f0"; }
+/******/ 		__webpack_require__.h = function() { return "294f222739e150b1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
